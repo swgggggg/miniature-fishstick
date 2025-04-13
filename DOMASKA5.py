2196631d1eb3105f51600c9a34c1fd40e906ba95
swgggggg <daniilberezuk79@gmail.com>
2025-04-13 17:23:12 +0200

    DOMASKA5

---
+++ DOMASKA5.py
@@ -0,0 +1,9 @@
+from colorama import init, Fore, Back, Style # модулі\
+print(Fore.RED, Back.WHITE,Style.BRIGHT + 'Hello!!')
+print(Fore.BLUE, Back.YELLOW,Style.DIM + 'Hello!!')
+print(Fore.GREEN, Back.RESET,Style.BRIGHT+ 'Hello!!')
+print(Fore.YELLOW, Back.BLUE,Style.DIM + 'Hello!!')
+print(Fore.WHITE, Back.RED,Style.BRIGHT + 'Hello!!')
+
+# colorama це бібліотека за допомогою якої можна змінювати колір, стиль, задній фон тексту
+
